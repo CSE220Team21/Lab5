061c0521920fafc0f7ab7b256b12c4d676fa6d70
MMayr15 <MMayr15@users.noreply.github.com>
2014-04-26 12:48:34 -0700

    Update Identifier.cpp

--- Identifier.cpp
+++ Identifier.cpp
@@ -1,9 +1,9 @@
-#=============================================================
-#	Mike Mayr (MMayr15), Chalie Crow (LevenBravo)
-#	Team 21
-#	https://github.com/CSE220Team21/Lab5
-#	Identifier.cpp
-#=============================================================
+/*==============================================================*/
+/*	Mike Mayr (MMayr15), Chalie Crow (LevenBravo)		*/
+/*	Team 21							*/
+/*	https://github.com/CSE220Team21/Lab5			*/
+/*	Identifier.cpp						*/
+/*==============================================================*/
 
 #include "Identifier.h"
 
