5513437192ff16eb4a93857653168453c699fc1d
MMayr15 <MMayr15@users.noreply.github.com>
2014-04-27 16:18:35 -0700

    Create EpicQuotes.h

---
+++ EpicQuotes.h
@@ -0,0 +1,34 @@
+/*==============================================================*/
+/*	Mike Mayr (MMayr15), Chalie Crow (LevenBravo)	             	*/
+/*	Team 21						                                        	*/
+/*	https://github.com/CSE220Team21/Lab5		                   	*/
+/*	EpicQuotes.h                                    						*/
+/*==============================================================*/
+#ifndef STRING_H_
+#define STRING_H_
+#include "Token.h"
+
+class EpicQuotes : public Token
+{
+
+	public:
+		EpicQuotes() : Token()
+		{
+			this->setCode(STRING);
+
+		}
+		~EpicQuotes()
+		{
+
+		}
+		void print()
+		{
+			char line[MAX_SOURCE_LINE_LENGTH + 32];
+			const char *symbol_string = SYMBOL_STRINGS[this->getCode()];
+
+			sprintf(line, "    >> %-16s %s\n", symbol_string, this->getTokenString().c_str());
+			printf("%s", line);
+		}
+};
+
+#endif
