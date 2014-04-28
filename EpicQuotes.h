3e19036a506b6e05722832a18f914914c3874a03
LevenBravo <charles.j.crow@gmail.com>
2014-04-27 19:33:08 -0700

    Update EpicQuotes.h

--- EpicQuotes.h
+++ EpicQuotes.h
@@ -1,8 +1,8 @@
 /*==============================================================*/
-/*	Mike Mayr (MMayr15), Chalie Crow (LevenBravo)	             	*/
-/*	Team 21						                                        	*/
-/*	https://github.com/CSE220Team21/Lab5		                   	*/
-/*	EpicQuotes.h                                    						*/
+/*	Mike Mayr (MMayr15), Chalie Crow (LevenBravo)	        */
+/*	Team 21						        */
+/*	https://github.com/CSE220Team21/Lab5		        */
+/*	EpicQuotes.h                               		*/
 /*==============================================================*/
 #ifndef STRING_H_
 #define STRING_H_
